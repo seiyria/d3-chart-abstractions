89add097ff2e882741cad5b9eb00907ec6669c7b
Kyle Kemp <kyle@seiyria.com>
2013-10-30 13:37:58 -0500

    adding sample data

---
+++ data/bar-data.js
@@ -0,0 +1,41 @@
+
+var legend = [
+    {name: "A", color: "#09b0d6"},
+    {name: "B", color: "#f99d24"},
+    {name: "C", color: "#5bac48"},
+    {name: "X", color: "#f8d208"},
+    {name: "Y", color: "#d72d2f"}
+];
+
+var semData = {
+    0: {
+        name: "Bar One",
+        data: [
+            {name: "A", value: 240},
+            {name: "B", value: 177},
+            {name: "C", value: 127},
+            {name: "X", value: 45},
+            {name: "Z", value: 11}
+        ]
+    },
+    1: {
+        name: "Bar Two",
+        data: [
+            {name: "A", value: 391},
+            {name: "B", value: 301},
+            {name: "C", value: 160},
+            {name: "X", value: 0},
+            {name: "Z", value: 74}
+        ]
+    },
+    2: {
+        name: "Bar Three",
+        data: [
+            {name: "A", value: 6},
+            {name: "B", value: 1},
+            {name: "C", value: 1},
+            {name: "X", value: 1, hidden: true},
+            {name: "Z", value: 0, hidden: true}
+        ]
+    }
+};
