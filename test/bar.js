de88be4ecc221693234d54bc6ac9e8727bedcfd7
Kyle Kemp <kyle@seiyria.com>
2013-11-06 14:50:17 -0600

    working on unit tests for the bar graph

---
+++ test/bar.js
@@ -0,0 +1,10 @@
+
+var d3 = require('d3');
+var chai = require('chai');
+var expect = chai.expect;
+chai.should();
+
+var bargraphs = require('../dist/development/bar.js');
+
+var x = new bargraphs.BarGraph({});
+
