cd1d232618c5318eaa103f727de85a42b19d6ec3
Kyle Kemp <kyle@seiyria.com>
2013-11-07 15:16:02 -0600

    updated unit tests

--- test/merge.js
+++ test/merge.js
@@ -2,7 +2,7 @@
 var assert = require('assert');
 
 var merger = require('../src/merge.js').Merge;
-
+var bargraph = require('../dist/development/bar.js').BarGraph;
 
 describe('Merge', function() {
 	describe('Flat Merge', function() {
@@ -79,6 +79,239 @@ describe('Merge', function() {
 
 		it('should not touch values that are in a nested object in both sides', function() {
 			assert.equal(merged.obj.untouched, 2);
+		});
+	});
+
+	describe('Bar Graph Merging', function() {
+
+		var defaults = bargraph.prototype.defaults;
+
+		it('should have a correctly calculated width (width-margin.left-margin.right)', function() {
+			var custom = {
+				layout: {
+					margin: {
+						top:    20,
+						bottom: 40,
+						left:   50,
+						right:  60
+					},
+					width:      960,
+					height:     500
+				}
+			};
+
+			var merged = merger.BarMerge(defaults, custom);
+
+			assert.equal(merged.layout.width, 850);
+		});
+
+		it('should have a correctly calculated height (height-margin.top-margin.bottom)', function() {
+			var custom = {
+				layout: {
+					margin: {
+						top:    20,
+						bottom: 40,
+						left:   50,
+						right:  60
+					},
+					width:      960,
+					height:     500
+				}
+			};
+
+			var merged = merger.BarMerge(defaults, custom);
+
+			assert.equal(merged.layout.height, 440);
+		});
+
+		it('should use the default formatting when none is supplied', function() {
+
+			var merged = merger.BarMerge(defaults, {});
+
+			assert.equal(merged.formatting.strFormat, "%n");
+			assert.equal(merged.display.labels.aggregate.formatting.strFormat, "%n");
+
+			assert.equal(merged.display.labels.tooltip.formatting.strFormat, "%n");
+			assert.equal(merged.display.labels.legend.formatting.strFormat, "%n");
+			assert.equal(merged.display.labels.axes.formatting.strFormat, "%n");
+			assert.equal(merged.display.labels.bars.formatting.strFormat, "%n");
+			assert.equal(merged.display.labels.x.formatting.strFormat, "%n");
+			assert.equal(merged.display.labels.y.formatting.strFormat, "%n");
+		});
+
+		it('should use a global formatting when no custom formatting is supplied', function() {
+
+			var custom = {
+				formatting: {
+					strFormat: "+%n"
+				}
+			};
+
+			var merged = merger.BarMerge(defaults, custom);
+
+			assert.equal(merged.formatting.strFormat, "+%n");
+			assert.equal(merged.display.labels.aggregate.formatting.strFormat, "+%n");
+			assert.equal(merged.display.labels.tooltip.formatting.strFormat, "+%n");
+			assert.equal(merged.display.labels.legend.formatting.strFormat, "+%n");
+			assert.equal(merged.display.labels.axes.formatting.strFormat, "+%n");
+			assert.equal(merged.display.labels.bars.formatting.strFormat, "+%n");
+			assert.equal(merged.display.labels.x.formatting.strFormat, "+%n");
+			assert.equal(merged.display.labels.y.formatting.strFormat, "+%n");
+
+		});
+
+		it('should use the custom formatting supplied instead of the global formatting', function() {
+
+			var custom = {
+				formatting: {
+					strFormat: "+%n"
+				},
+
+				display: {
+					labels: {
+						aggregate: {
+							formatting: {
+								numFormat: '',
+								strFormat: '++%n'
+							}
+						}
+					}
+				}
+			};
+
+			var merged = merger.BarMerge(defaults, custom);
+
+			assert.equal(merged.formatting.strFormat, "+%n");
+			assert.equal(merged.display.labels.aggregate.formatting.strFormat, "++%n");
+
+			assert.equal(merged.display.labels.tooltip.formatting.strFormat, "+%n");
+			assert.equal(merged.display.labels.legend.formatting.strFormat, "+%n");
+			assert.equal(merged.display.labels.axes.formatting.strFormat, "+%n");
+			assert.equal(merged.display.labels.bars.formatting.strFormat, "+%n");
+			assert.equal(merged.display.labels.x.formatting.strFormat, "+%n");
+			assert.equal(merged.display.labels.y.formatting.strFormat, "+%n");
+
+		});
+
+		it('should use the default font when none is supplied', function() {
+
+			var merged = merger.BarMerge(defaults, {});
+
+			assert.equal(merged.font.face, "sans-serif");
+			assert.equal(merged.display.labels.aggregate.font.face, "sans-serif");
+
+			assert.equal(merged.display.labels.tooltip.font.face, "sans-serif");
+			assert.equal(merged.display.labels.legend.font.face, "sans-serif");
+			assert.equal(merged.display.labels.axes.font.face, "sans-serif");
+			assert.equal(merged.display.labels.bars.font.face, "sans-serif");
+			assert.equal(merged.display.labels.x.font.face, "sans-serif");
+			assert.equal(merged.display.labels.y.font.face, "sans-serif");
+		});
+
+		it('should use the global font when there are overridden properties', function() {
+
+			var custom = {
+				font: {
+					face: "Times"
+				}
+			};
+
+			var merged = merger.BarMerge(defaults, custom);
+
+			assert.equal(merged.font.face, "Times");
+			assert.equal(merged.display.labels.aggregate.font.face, "Times");
+
+			assert.equal(merged.display.labels.tooltip.font.face, "Times");
+			assert.equal(merged.display.labels.legend.font.face, "Times");
+			assert.equal(merged.display.labels.axes.font.face, "Times");
+			assert.equal(merged.display.labels.bars.font.face, "Times");
+			assert.equal(merged.display.labels.x.font.face, "Times");
+			assert.equal(merged.display.labels.y.font.face, "Times");
+		});
+
+		it('should use a custom font instead of the global font when specified', function() {
+
+			var custom = {
+				font: {
+					face: "Times"
+				},
+
+				display: {
+					labels: {
+						aggregate: {
+							font: {
+								face: "Courier"
+							}
+						}
+					}
+				}
+			};
+
+			var merged = merger.BarMerge(defaults, custom);
+
+			assert.equal(merged.font.face, "Times");
+			assert.equal(merged.display.labels.aggregate.font.face, "Courier");
+
+			assert.equal(merged.display.labels.tooltip.font.face, "Times");
+			assert.equal(merged.display.labels.legend.font.face, "Times");
+			assert.equal(merged.display.labels.axes.font.face, "Times");
+			assert.equal(merged.display.labels.bars.font.face, "Times");
+			assert.equal(merged.display.labels.x.font.face, "Times");
+			assert.equal(merged.display.labels.y.font.face, "Times");
+		});
+
+		it('should copy the bar formatting to the tooltip formatting when no tooltip formatting is supplied', function() {
+
+			var merged = merger.BarMerge(defaults, {});
+
+			assert.equal(merged.display.labels.tooltip.formatting.strFormat, "%n");
+			assert.equal(merged.display.labels.bars.formatting.strFormat, "%n");
+		});
+
+		it('should copy custom bar formatting to the tooltip formatting when no tooltip formatting is supplied', function() {
+			var custom = {
+				display: {
+					labels: {
+						bars: {
+							formatting: {
+								numFormat: '',
+								strFormat: '++%n'
+							}
+						}
+					}
+				}
+			};
+
+			var merged = merger.BarMerge(defaults, custom);
+
+			assert.equal(merged.display.labels.tooltip.formatting.strFormat, "++%n");
+			assert.equal(merged.display.labels.bars.formatting.strFormat, "++%n");
+		});
+
+		it('should not copy any formatting to the tooltip formatting when it is supplied', function() {
+			var custom = {
+				display: {
+					labels: {
+						bars: {
+							formatting: {
+								numFormat: '',
+								strFormat: '+%n'
+							}
+						},
+						tooltip: {
+							formatting: {
+								numFormat: '',
+								strFormat: '++%n'
+							}
+						}
+					}
+				}
+			};
+
+			var merged = merger.BarMerge(defaults, custom);
+
+			assert.equal(merged.display.labels.tooltip.formatting.strFormat, "++%n");
+			assert.equal(merged.display.labels.bars.formatting.strFormat, "+%n");
 		});
 	});
 });
