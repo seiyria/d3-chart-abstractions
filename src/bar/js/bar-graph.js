5fd3b727f95b4d10e8d90c83a2ba4be79e757f69
Kyle Kemp <kyle@seiyria.com>
2013-11-07 12:14:35 -0600

    gave my code a proper refactoring for testing

--- src/bar/js/bar-graph.js
+++ src/bar/js/bar-graph.js
@@ -2,47 +2,7 @@
 var BarGraph = function(opts) {
 
 	//merge the objects, taking only the data from opts
-	var merged = this.merge(this.defaults, opts);
-
-	merged.layout.width = merged.layout.width - merged.layout.margin.left - merged.layout.margin.right;
-	merged.layout.height = merged.layout.height - merged.layout.margin.top - merged.layout.margin.bottom;
-
-	//assign global formatting if necessary
-	var formattingObjects = [
-		merged.display.labels.aggregate,
-		merged.display.labels.tooltip,
-		merged.display.labels.legend,
-		merged.display.labels.axes,
-		merged.display.labels.bars,
-		merged.display.labels.x,
-		merged.display.labels.y
-	];
-
-	merged.display.labels.tooltip.formatting = merged.display.labels.tooltip.formatting || merged.display.labels.bars.formatting;
-
-	formattingObjects.forEach(function(e, i) {
-		e.formatting = e.formatting || merged.formatting;
-
-		if(typeof e.formatting.numFormat !== "function")
-			e.formatting.numFormat = d3.format(e.formatting.numFormat);
-	});
-
-	//generally we want the tooltip formatting to be the same as the bar... but you never know I guess
-	
-	//do the same thing as the formatting objects
-	var fontObjects = [
-		merged.display.labels.aggregate,
-		merged.display.labels.tooltip,
-		merged.display.labels.legend,
-		merged.display.labels.axes,
-		merged.display.labels.bars,
-		merged.display.labels.x,
-		merged.display.labels.y
-	];
-
-	fontObjects.forEach(function(e, i) {
-		e.font = e.font || merged.font;
-	});
+	var merged = Merge.BarMerge(this.defaults, opts);
 
 	//this is the only data that needs to be accessible while rendering, since the rest was computed
 	this.layout = merged.layout;
@@ -954,23 +914,4 @@ BarGraph.prototype.defaults = {
 	}
 };
 
-BarGraph.prototype.merge = function(left, right) {
-
-	//aww yeah, deep copies
-	var ret = JSON.parse(JSON.stringify(left));
-
-	for(var property in left) {
-		if(right.hasOwnProperty(property)) {
-			if(typeof left[property] === "object")
-				ret[property] = this.merge(left[property], right[property]);
-			else
-				ret[property] = right[property];
-		}
-	}
-
-	return ret;
-};
-
 BarGraph.prototype.currentItem = 0;
-
-exports.BarGraph = BarGraph;
--- src/bar/js/horiz-bar-graph.js
+++ src/bar/js/horiz-bar-graph.js
@@ -2,45 +2,7 @@
 var HorizontalBarGraph = function(opts) {
 
 	//merge the objects, taking only the data from opts
-	var merged = this.merge(this.defaults, opts);
-
-	merged.layout.width = merged.layout.width - merged.layout.margin.left - merged.layout.margin.right;
-	merged.layout.height = merged.layout.height - merged.layout.margin.top - merged.layout.margin.bottom;
-
-	//assign global formatting if necessary
-	var formattingObjects = [
-		merged.display.labels.aggregate,
-		merged.display.labels.tooltip,
-		merged.display.labels.legend,
-		merged.display.labels.axes,
-		merged.display.labels.bars,
-		merged.display.labels.x,
-		merged.display.labels.y
-	];
-
-	merged.display.labels.tooltip.formatting = merged.display.labels.tooltip.formatting || merged.display.labels.bars.formatting;
-
-	formattingObjects.forEach(function(e, i) {
-		e.formatting = e.formatting || merged.formatting;
-
-		if(typeof e.formatting.numFormat !== "function")
-			e.formatting.numFormat = d3.format(e.formatting.numFormat);
-	});
-	
-	//do the same thing as the formatting objects
-	var fontObjects = [
-		merged.display.labels.aggregate,
-		merged.display.labels.tooltip,
-		merged.display.labels.legend,
-		merged.display.labels.axes,
-		merged.display.labels.bars,
-		merged.display.labels.x,
-		merged.display.labels.y
-	];
-
-	fontObjects.forEach(function(e, i) {
-		e.font = e.font || merged.font;
-	});
+	var merged = Merge.BarMerge(this.defaults, opts);
 
 	//this is the only data that needs to be accessible while rendering, since the rest was computed
 	this.layout = merged.layout;
@@ -935,23 +897,4 @@ HorizontalBarGraph.prototype.defaults =  {
 	}
 };
 
-HorizontalBarGraph.prototype.merge = function(left, right) {
-
-	//aww yeah, deep copies
-	var ret = JSON.parse(JSON.stringify(left));
-
-	for(var property in left) {
-		if(right.hasOwnProperty(property)) {
-			if(typeof left[property] === "object")
-				ret[property] = this.merge(left[property], right[property]);
-			else
-				ret[property] = right[property];
-		}
-	}
-
-	return ret;
-};
-
 HorizontalBarGraph.prototype.currentItem = 0;
-
-exports.HorizontalBarGraph = HorizontalBarGraph;
